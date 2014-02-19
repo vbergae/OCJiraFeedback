11821cc973710c4fb35c7f2957ea7f1d2d651f93
Víctor Berga <vbergae@gmail.com>
2014-02-19 19:11:15 +0100

    Extends documentation on OCConnectionManager

--- src/OCJiraFeedback/OCJiraFeedback/Model/OCConnectionManager.h
+++ src/OCJiraFeedback/OCJiraFeedback/Model/OCConnectionManager.h
@@ -11,12 +11,46 @@
 
 @class OCJiraIssue;
 
+/**
+ OCConnectionManager
+ 
+ Singleton class used as bridge between your Jira instance and the app. Has
+ as main reponsability configure connection settings (loading from the
+ Instance.plist file) and perform HTTP requests.
+ */
 @interface OCConnectionManager : AFHTTPRequestOperationManager
 
+/**
+ @name Properties
+ */
+
+/**
+ Default issue type from Instance.plist settings. Configure it based
+ on your Jira configuration.
+ 
+ Issue will be create in the ser using this type
+ */
 @property (readonly) NSString *issueType;
 
+/**
+ @name Instance methods
+ */
+
+/**
+ Saves the given issue on your server
+ 
+ @param issue New issue to be created
+ @param handler Completion handler
+ */
 - (void)save:(OCJiraIssue *)issue completion:(void(^)(NSError *))handler;
 
+/**
+ @name Class methods
+ */
+
+/**
+ Returns an instancen configured from the attributes on Instance.plist file
+ */
 + (instancetype)sharedManager;
 
 @end

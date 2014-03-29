554c182cce525c8dd6f3217b517616399dba3c64
Víctor Berga <vbergae@gmail.com>
2014-03-29 13:15:45 +0100

    Defines initial interface for OCRequest

--- Classes/OCRequest.h
+++ Classes/OCRequest.h
@@ -6,8 +6,70 @@
 //  Copyright (c) 2014 VictorBerga.com. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+/**
+ Indicates the method used in the request.
+ 
+ Use this constant to set the requestMethod property.
+ */
+typedef NS_ENUM(unsigned char, OCRequestMethod) {
+    OCRequestMethodGET,
+    OCRequestMethodPOST
+};
 
 @interface OCRequest : NSObject
+
+/**
+ @name Properties
+ */
+
+/**
+ The destination's relative path for this request.
+ */
+@property (readonly) NSString *path;
+
+/**
+ The method to use for this request.
+ */
+@property (readonly) OCRequestMethod requestMethod;
+
+/**
+ The parameters to use for this request
+ */
+@property (readonly) NSDictionary *parameters;
+
+/**
+ @name Initializing Requests
+ */
+
+/**
+ Default initializer
+ 
+ Creates a request instance using the path, parameters (if exists) and
+ request method.
+ 
+ @param path Relative path
+ @param parameters 
+ @param requestMethod
+ 
+ @return Request instance
+ */
+- (id)initWithPath:(NSString *)path
+        paremeters:(NSDictionary *)parameters
+     requestMethod:(OCRequestMethod)requestMethod;
+
+/**
+ @name Sending Requests
+ */
+
+/**
+ Performs the request and calls the specified handler when done.
+ 
+ @param handler The handler to call when the request is done. This
+ handler will be called using main thread.
+ */
+- (void)performRequestWithHandler:(void(^)(id responseObject,
+                                           NSError *error))handler;
 
 @end
